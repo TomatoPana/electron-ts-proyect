dae828960775f4e5ab5455b90a78adf5cf5816ab
Moises David Lozano Bobadilla <mdlb.lobo@gmail.com>
2023-03-01 00:14:02 -0600

    feat: Drag Elements Added

--- src/renderer.ts
+++ src/renderer.ts
@@ -7,6 +7,11 @@ let selected: HTMLElement | null = null;
 var number = 64;
 
 drawArea.addEventListener("click", (event: MouseEvent) => {
+  if (selected !== null) {
+    selected.style.borderWidth = "1px";
+    selected = null;
+    return;
+  }
   const element = document.createElement("div");
   console.log(event);
   element.style.left = (event.clientX - 15).toString() + "px";
@@ -32,6 +37,43 @@ drawArea.addEventListener("click", (event: MouseEvent) => {
     }
     element.style.borderWidth = "3px";
     selected = element;
+  });
+
+  let newPosX = 0,
+    newPosY = 0,
+    startPosX = 0,
+    startPosY = 0;
+  function mouseMove(event: MouseEvent) {
+    // calculate the new position
+    newPosX = startPosX - event.clientX;
+    newPosY = startPosY - event.clientY;
+
+    // with each move we also want to update the start X and Y
+    startPosX = event.clientX;
+    startPosY = event.clientY;
+
+    // set the element's new position:
+    element.style.top = element.offsetTop - newPosY + "px";
+    element.style.left = element.offsetLeft - newPosX + "px";
+  }
+  element.addEventListener("mousedown", (event) => {
+    event.preventDefault();
+    event.stopPropagation();
+    if (selected !== null) {
+      // Se crea una union entre los elementos
+      selected.style.borderWidth = "1px";
+      selected = null;
+    }
+    element.style.borderWidth = "3px";
+    selected = element;
+    startPosX = event.clientX;
+    startPosY = event.clientY;
+
+    document.addEventListener("mousemove", mouseMove);
+
+    document.addEventListener("mouseup", function () {
+      document.removeEventListener("mousemove", mouseMove);
+    });
   });
 
   drawArea.appendChild(element);
