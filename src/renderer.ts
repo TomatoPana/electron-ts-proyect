fd9e38d64ca24e9c9625f9431d1a520936deb1dc
Moises David Lozano Bobadilla <mdlb.lobo@gmail.com>
2023-02-28 23:56:59 -0600

    wip: Improved Click to add and added remove node

--- src/renderer.ts
+++ src/renderer.ts
@@ -1,22 +1,16 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// No Node.js APIs are available in this process unless
-// nodeIntegration is set to true in webPreferences.
-// Use preload.js to selectively enable features
-// needed in the renderer process.
-
 const drawArea = document.getElementById("drawArea");
 
 const nodes = [];
 
 let selected: HTMLElement | null = null;
 
-drawArea.addEventListener("click", (event) => {
+var number = 64;
+
+drawArea.addEventListener("click", (event: MouseEvent) => {
   const element = document.createElement("div");
-  console.log("Left: %d", (event.clientX - 15).toString());
-  console.log("Top: %d", (event.clientY - 115).toString());
+  console.log(event);
   element.style.left = (event.clientX - 15).toString() + "px";
-  element.style.top = (event.clientY - 115).toString() + "px";
+  element.style.top = (event.clientY - 150).toString() + "px";
   element.style.borderRadius = "50%";
   element.style.border = "black solid 1px";
   element.style.width = "30px";
@@ -26,16 +20,38 @@ drawArea.addEventListener("click", (event) => {
   element.style.justifyContent = "center";
   element.style.display = "flex";
   element.style.zIndex = "2";
+  element.style.background = "#ccc";
+  element.style.userSelect = "none";
   element.innerText = "A";
   element.addEventListener("click", (event) => {
     event.stopPropagation();
     if (selected !== null) {
       // Se crea una union entre los elementos
       selected.style.borderWidth = "1px";
+      selected = null;
     }
     element.style.borderWidth = "3px";
     selected = element;
   });
 
   drawArea.appendChild(element);
+});
+
+document.addEventListener("keyup", (event) => {
+  switch (event.code) {
+    case "Escape":
+      event.preventDefault();
+      if (selected !== null) {
+        selected.style.borderWidth = "1px";
+        selected = null;
+      }
+      break;
+    case "Delete":
+      event.preventDefault();
+      if (selected !== null) {
+        drawArea.removeChild(selected);
+        selected = null;
+      }
+      break;
+  }
 });
